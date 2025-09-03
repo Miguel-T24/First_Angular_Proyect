6f37481491e278287d0ec89908077d4efc8ee407
Miguel <miguelt2296@gmail.com>
2025-09-03 01:07:14 -0500

    Practicas con event binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,6 +14,30 @@ export class App {
   name = "Miguel";
   apellido = "Tamayo";
   age = 29;
+  isActive = false ? "Activo" : "Inactivo"
+  imageURL = "https://images7.alphacoders.com/133/1338182.png";
 
-  isActive = true
+  // Variables para los ejercicios de binding
+  username = "Angular Student";
+
+  // Habilitar o desabiulitar un boton con binging
+  isDisable = false;
+  onClick(){
+  console.log("Boton Click");
+  };
+
+    // Agregando una imagen
+  image2 = "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg";
+
+  // Cambiando el color dependiendo de una variable
+  isRed = true;
+
+  // Clases Dinamicas
+  isActive1 = true;
+
+  // Mostrar varias imagenes desde un array
+  images = [
+    "https://m.media-amazon.com/images/M/MV5BMjA5NzU2MDYzMF5BMl5BanBnXkFtZTcwODIzNDc3Mw@@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMTk0NjcxOTE1MF5BMl5BanBnXkFtZTYwMjc0NTg3._V1_.jpg"
+  ];
 }
